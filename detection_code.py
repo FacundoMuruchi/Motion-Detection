ac40435a5c6808522bee41fa4ed0732a6343e88e
Facundo Muruchi <facundomuruchi01@gmail.com>
2025-03-02 18:25:47 -0300

    feat: Join near areas

--- detection_code.py
+++ detection_code.py
@@ -42,15 +42,16 @@ while True:
         
         # Motion Detection
         frame_diff = cv2.absdiff(prev_gray, gray)
-        blurred = cv2.GaussianBlur(frame_diff, (5, 5), 0)
+        blurred = cv2.GaussianBlur(frame_diff, (7, 7), 0)
         _, thresh = cv2.threshold(blurred, 15, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=3)  # join near areas
         
         # Bounding Boxes
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         frame_with_contours = frame_resized.copy()
         
         for contour in contours:
-            if cv2.contourArea(contour) > 2000:
+            if cv2.contourArea(contour) > 1200:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(frame_with_contours, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
